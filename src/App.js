73cef2784f7c3ad6af8f8c76b5a9f9f4b52ab1e3
DhaneshDDK <dandedhanesh899@gmail.com>
2023-12-21 15:25:38 +0530

    Commit-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { fetchAllProducts } from './Services/Operations/Product';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setProducts } from './Redux/Slices/ProductSlice';
+import Navbar from './Componenets/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import { setLoading } from './Redux/Slices/ProductSlice';
+import LoginPage from './Pages/LoginPage';
+import SingUpPage from './Pages/SignUpPage';
+import { useSelector } from 'react-redux';
+import { setUserData } from './Redux/Slices/userSlice';
+import { verifyUserToken } from './Services/Operations/User';
+import Cart from './Pages/Cart';
 
 function App() {
+  const dispatch = useDispatch();
+  const {token} = useSelector((state)=>state.auth);
+  // console.log(token);
+  useEffect(()=>{
+    const fetchData = async ()=>{ 
+      dispatch(setLoading(true));
+      try {
+         const data = await fetchAllProducts();
+         dispatch(setProducts(data?.data?.response));
+      } catch (error) {
+         console.log(error);
+      }
+      dispatch(setLoading(false));
+    }
+
+
+   const checkToken = async ()=>{
+    
+      try { 
+         const res = await verifyUserToken(token);
+        //  console.log(res.data.data)
+         if(res?.data?.success) dispatch(setUserData(res?.data?.data));
+      } catch (error) {
+         console.log(error);
+      }
+   
+   }
+
+    if(token) checkToken();
+    fetchData();
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar/>
+      <Routes>
+         <Route path="/" element = {<Home/>} />
+         <Route path="/login" element= {<LoginPage/>}/>
+         <Route path="/signup" element= {<SingUpPage/> }/>
+         <Route path="/cart" element= {<Cart/>}/>
+      </Routes>
     </div>
   );
 }
